8d4b535c2a0b5ba948fc32360cd054a0e981033d
Honghanh <hanhdthds@gmail.com>
2023-10-15 10:00:27 +0700

    Update environtment

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,8 +5,9 @@
 export const environment = {
   production: false,
   appName: 'Udagram microservice local',
-  apiHost: 'http://localhost:8080/api/v0'
+  // apiHost: 'http://localhost:8080/api/v0'
   // apiHost: 'ad2bf8c1a9f644c4abf99d79814666ae-1339922253.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'a56617c87e88745e8b57ef087809b5bd-319352329.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
